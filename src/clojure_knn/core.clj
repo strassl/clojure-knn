21801fd19fb07486fc08f4aae2ed20726229373c
Simon Strassl <stuff@sigmoid.at>
2017-04-25 14:38:48 +0200

    Add validation

--- src/clojure_knn/core.clj
+++ src/clojure_knn/core.clj
@@ -35,18 +35,32 @@
   [a b]
   (norm (map - a b)))
 
-
 (defn read-data
   [path]
   (map (fn [row] (Sample. (into [] (map read-string (butlast row)))
                           (last row)))
        (parse-csv (slurp path))))
 
+(defn loss
+  [classify, validation]
+  (let [classify-sample (fn [sample] (classify (:location sample)))
+        correct-class (fn [sample] (if (= (classify-sample sample)(:label sample)) true false))
+        results (frequencies (map correct-class validation))
+        incorrect (get results false)
+        correct (get results true)
+        incorrect-ratio (/ incorrect (+ correct incorrect))
+        ]
+    incorrect-ratio))
+
 (defn -main
   [& args]
-  (let [path (nth *command-line-args* 0)
-        k (read-string (nth *command-line-args* 1))
-        sample (read-string (nth *command-line-args* 2))
-        population (read-data path)
+  (let [train-path (nth *command-line-args* 0)
+        val-path (nth *command-line-args* 1)
+        k (read-string (nth *command-line-args* 2))
+        sample (read-string (nth *command-line-args* 3))
+        validation (read-data val-path)
+        population (read-data train-path)
+        val-loss (loss (fn [sample] (apply classify k vec-dist sample population)) validation)
         result (apply classify k vec-dist sample population)]
-    (println result)))
+    (do (println result)
+        (println val-loss))))
